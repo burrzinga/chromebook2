78ab75d47ad6bcc6906158c997e5f3ccf43550c1
Burrzinga Sutter <burrzinga@gmail.com>
2018-02-09 00:44:26 +0000

    bonjourx

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  static final String template = "Bonjour, %s!";
+  static final String template = "BonjourX, %s!";
 
   @Override
   public void start(Future<Void> future) {
